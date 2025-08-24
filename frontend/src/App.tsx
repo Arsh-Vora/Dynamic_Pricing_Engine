a431ced0017e4f658c47900394145c28740c44fd
Arsh Vora <arshsajidvora@gmail.com>
2025-08-25 00:47:48 +0200

    Add base CSS styles and main App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import ProductSubmissionForm from './components/ProductSubmissionForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductSubmissionForm />
+    </div>
+  );
+}
+
+export default App;
